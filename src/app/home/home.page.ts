a220a7f27d690c307c7116c2b38431c95758157e
smurray140402 <g00426904@atu.ie>
2025-04-14 18:51:02 +0100

    Designed the home page and implemented a search bar and button

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,22 @@
 import { Component } from '@angular/core';
-import { IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/angular/standalone';
+import { IonHeader, IonToolbar, IonTitle, IonContent, IonSearchbar, IonButton } from '@ionic/angular/standalone';
+import { FormsModule } from '@angular/forms'; 
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
-  imports: [IonHeader, IonToolbar, IonTitle, IonContent],
+  imports: [IonHeader, IonToolbar, IonTitle, IonContent, IonSearchbar, IonButton, FormsModule],
 })
 export class HomePage {
+
+  // Variable to hold the city name
+  city: string = "";
+
   constructor() {}
+
+  // Function called when search button is clicked
+  searchWeather() {
+    console.log('Searching weather for: ', this.city);
+  }
 }
