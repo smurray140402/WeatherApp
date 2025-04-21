c3cdfa0ad48696bf8fff4a33184c8363a6bb29c9
smurray140402 <g00426904@atu.ie>
2025-04-21 11:32:20 +0100

    Refactored code to use provideHttpClient instead of HttpClientModule

--- src/app/weather-info/weather-info.page.ts
+++ src/app/weather-info/weather-info.page.ts
@@ -3,14 +3,14 @@ import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { IonContent, IonHeader, IonTitle, IonToolbar, IonButtons, IonBackButton } from '@ionic/angular/standalone';
 import { WeatherDataService } from '../weather-data.service';
-import { HttpClientModule, HttpClient } from '@angular/common/http';
+import { provideHttpClient, HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-weather-info',
   templateUrl: './weather-info.page.html',
   styleUrls: ['./weather-info.page.scss'],
   standalone: true,
-  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule, IonButtons, IonBackButton, HttpClientModule]
+  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule, IonButtons, IonBackButton]
 })
 export class WeatherInfoPage {
 
