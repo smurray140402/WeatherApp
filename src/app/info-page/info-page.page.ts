1d9d6d9bb4b6a64eb8f08fc86fb2a93869f8f138
smurray140402 <g00426904@atu.ie>
2025-04-23 13:29:37 +0100

    Added 'How Weather Works' and 'Did You Know?' sections and enhanced design of the help/info page

--- src/app/info-page/info-page.page.ts
+++ src/app/info-page/info-page.page.ts
@@ -2,13 +2,15 @@ import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { IonContent, IonHeader, IonTitle, IonToolbar, IonButtons, IonBackButton } from '@ionic/angular/standalone';
+import { IonCard, IonCardContent, IonCardHeader, IonCardSubtitle, IonCardTitle, IonItem, IonList } from '@ionic/angular/standalone';
+
 
 @Component({
   selector: 'app-info-page',
   templateUrl: './info-page.page.html',
   styleUrls: ['./info-page.page.scss'],
   standalone: true,
-  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule, IonButtons, IonBackButton]
+  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule, IonButtons, IonBackButton, IonCard, IonCardContent, IonCardHeader, IonCardSubtitle, IonCardTitle, IonItem, IonList]
 })
 export class InfoPagePage implements OnInit {
 
